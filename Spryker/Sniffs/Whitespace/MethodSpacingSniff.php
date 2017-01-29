99c78d26617eebd9250fafc99a8a740cb75dde10
mscherer <mark.scherer@spryker.com>
2017-01-29 15:37:28 +0100

    Prepare 3.0 version of sniffer to run.

--- Spryker/Sniffs/Whitespace/MethodSpacingSniff.php
+++ Spryker/Sniffs/Whitespace/MethodSpacingSniff.php
@@ -85,13 +85,13 @@ class MethodSpacingSniff extends AbstractSprykerSniff
     }
 
     /**
-     * @param \PHP_CodeSniffer_File $phpcsFile
+     * @param \PHP_CodeSniffer\Files\File $phpcsFile
      * @param int $from
      * @param int $to
      *
      * @return void
      */
-    protected function assertNoAdditionalNewlinesForEmptyBody(PHP_CodeSniffer_File $phpcsFile, $from, $to)
+    protected function assertNoAdditionalNewlinesForEmptyBody(File $phpcsFile, $from, $to)
     {
         $tokens = $phpcsFile->getTokens();
 
