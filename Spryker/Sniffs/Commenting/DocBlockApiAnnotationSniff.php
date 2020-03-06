7f9bb1db9f52238f8f4b8761596738fd4e47a19b
mscherer <mark.scherer@spryker.com>
2020-03-06 16:44:21 +0100

    Fix inheritdoc and api method spec.

--- Spryker/Sniffs/Commenting/DocBlockApiAnnotationSniff.php
+++ Spryker/Sniffs/Commenting/DocBlockApiAnnotationSniff.php
@@ -8,13 +8,15 @@
 namespace Spryker\Sniffs\Commenting;
 
 use PHP_CodeSniffer\Files\File;
-use PHP_CodeSniffer\Sniffs\Sniff;
+use Spryker\Sniffs\AbstractSniffs\AbstractApiClassDetectionSprykerSniff;
 
 /**
  * Checks if doc block of Spryker API classes (Client, Facade, QueryContainer, Plugin) contain `@api` annotations.
  */
-class DocBlockApiAnnotationSniff implements Sniff
+class DocBlockApiAnnotationSniff extends AbstractApiClassDetectionSprykerSniff
 {
+    protected const INHERIT_DOC = '{@inheritDoc}';
+
     /**
      * @inheritDoc
      */
@@ -30,189 +32,16 @@ class DocBlockApiAnnotationSniff implements Sniff
      */
     public function process(File $phpCsFile, $stackPointer)
     {
-        if (!$this->isSprykerApiClass($phpCsFile, $stackPointer) || !$this->isPublicMethod($phpCsFile, $stackPointer)) {
+        $apiClass = $this->sprykerApiClass($phpCsFile, $stackPointer);
+
+        if ($apiClass === null || !$this->isPublicMethod($phpCsFile, $stackPointer) || $this->isConstructor($phpCsFile, $stackPointer)) {
             // To be finalized once all plugins are detected properly.
             //$this->assertNoApiTag($phpCsFile, $stackPointer);
 
             return;
         }
 
-        $this->assertApiAnnotation($phpCsFile, $stackPointer);
-    }
-
-    /**
-     * @param \PHP_CodeSniffer\Files\File $phpCsFile
-     * @param int $stackPointer
-     *
-     * @return bool
-     */
-    protected function isSprykerApiClass(File $phpCsFile, int $stackPointer): bool
-    {
-        if (!$this->hasNamespace($phpCsFile, $stackPointer) || !$this->hasClassOrInterfaceName($phpCsFile, $stackPointer)) {
-            return false;
-        }
-
-        $namespace = $this->getNamespace($phpCsFile, $stackPointer);
-        $name = $this->getClassOrInterfaceName($phpCsFile, $stackPointer);
-
-        if (
-            $this->isFacade($namespace, $name)
-            || $this->isClient($namespace, $name)
-            || $this->isQueryContainer($namespace, $name)
-            || $this->isPlugin($namespace, $name)
-        ) {
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * @param \PHP_CodeSniffer\Files\File $phpCsFile
-     * @param int $stackPointer
-     *
-     * @return bool
-     */
-    protected function isPublicMethod(File $phpCsFile, int $stackPointer): bool
-    {
-        $publicPosition = $phpCsFile->findFirstOnLine(T_PUBLIC, $stackPointer);
-        if ($publicPosition) {
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * @param \PHP_CodeSniffer\Files\File $phpCsFile
-     * @param int $stackPointer
-     *
-     * @return bool
-     */
-    protected function hasNamespace(File $phpCsFile, int $stackPointer): bool
-    {
-        $namespacePosition = $phpCsFile->findPrevious(T_NAMESPACE, $stackPointer);
-        if (!$namespacePosition) {
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * @param \PHP_CodeSniffer\Files\File $phpCsFile
-     * @param int $stackPointer
-     *
-     * @return bool
-     */
-    protected function hasClassOrInterfaceName(File $phpCsFile, int $stackPointer): bool
-    {
-        $classOrInterfaceNamePosition = $phpCsFile->findPrevious([T_CLASS, T_INTERFACE], $stackPointer);
-        if (!$classOrInterfaceNamePosition) {
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * @param \PHP_CodeSniffer\Files\File $phpCsFile
-     * @param int $stackPointer
-     *
-     * @return string
-     */
-    protected function getNamespace(File $phpCsFile, int $stackPointer): string
-    {
-        $namespacePosition = $phpCsFile->findPrevious(T_NAMESPACE, $stackPointer);
-        $endOfNamespacePosition = $phpCsFile->findEndOfStatement($namespacePosition);
-
-        $tokens = $phpCsFile->getTokens();
-        $namespaceTokens = array_splice($tokens, $namespacePosition + 2, $endOfNamespacePosition - $namespacePosition - 2);
-
-        $namespace = '';
-        foreach ($namespaceTokens as $token) {
-            $namespace .= $token['content'];
-        }
-
-        return $namespace;
-    }
-
-    /**
-     * @param \PHP_CodeSniffer\Files\File $phpCsFile
-     * @param int $stackPointer
-     *
-     * @return string
-     */
-    protected function getClassOrInterfaceName(File $phpCsFile, int $stackPointer): string
-    {
-        $classOrInterfacePosition = $phpCsFile->findPrevious([T_CLASS, T_INTERFACE], $stackPointer);
-        $classOrInterfaceNamePosition = $phpCsFile->findNext(T_STRING, $classOrInterfacePosition);
-
-        return $phpCsFile->getTokens()[$classOrInterfaceNamePosition]['content'];
-    }
-
-    /**
-     * @param string $namespace
-     * @param string $name
-     *
-     * @return bool
-     */
-    protected function isFacade(string $namespace, string $name): bool
-    {
-        if (preg_match('/^Spryker[a-zA-Z]*\\\\Zed\\\\[a-zA-Z]+\\\\Business$/', $namespace) && preg_match('/^(.*?)(Facade|FacadeInterface)$/', $name)) {
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * @param string $namespace
-     * @param string $name
-     *
-     * @return bool
-     */
-    protected function isClient(string $namespace, string $name): bool
-    {
-        if (preg_match('/^Spryker[a-zA-Z]*\\\\Client\\\\[a-zA-Z]+$/', $namespace) && preg_match('/^(.*?)(Client|ClientInterface)$/', $name)) {
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * @param string $namespace
-     * @param string $name
-     *
-     * @return bool
-     */
-    protected function isPlugin(string $namespace, string $name): bool
-    {
-        if (preg_match('/^Spryker[a-zA-Z]*\\\\[a-zA-Z]+\\\\[a-zA-Z]+\\\\Dependency\\\\Plugin\b/', $namespace) && preg_match('/^\w+Interface$/', $name)) {
-            return true;
-        }
-
-        if (preg_match('/^Spryker[a-zA-Z]*\\\\[a-zA-Z]+\\\\[a-zA-Z]+\\\\Communication\\\\Plugin\b/', $namespace) && preg_match('/^\w+Plugin$/', $name)) {
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * @param string $namespace
-     * @param string $name
-     *
-     * @return bool
-     */
-    protected function isQueryContainer(string $namespace, string $name): bool
-    {
-        if (preg_match('/^Spryker[a-zA-Z]*\\\\Zed\\\(.*?)\\\\Persistence$/', $namespace) && preg_match('/^(.*?)(QueryContainer|QueryContainerInterface)$/', $name)) {
-            return true;
-        }
-
-        return false;
+        $this->assertApiAnnotation($phpCsFile, $stackPointer, $apiClass);
     }
 
     /**
@@ -228,9 +57,11 @@ class DocBlockApiAnnotationSniff implements Sniff
             return null;
         }
 
-        $docCommentClosingPosition = $phpCsFile->findNext(T_DOC_COMMENT_CLOSE_TAG, $docCommentOpenerPosition);
-
         $tokens = $phpCsFile->getTokens();
+        $docCommentClosingPosition = $tokens[$docCommentOpenerPosition]['comment_closer'];
+        if (!$docCommentClosingPosition) {
+            return null;
+        }
 
         for ($i = $docCommentOpenerPosition + 1; $i < $docCommentClosingPosition; $i++) {
             $docCommentToken = $tokens[$i];
@@ -245,17 +76,22 @@ class DocBlockApiAnnotationSniff implements Sniff
     /**
      * @param \PHP_CodeSniffer\Files\File $phpCsFile
      * @param int $stackPointer
+     * @param string|null $apiClass
      *
      * @return void
      */
-    protected function assertApiAnnotation(File $phpCsFile, int $stackPointer): void
+    protected function assertApiAnnotation(File $phpCsFile, int $stackPointer, ?string $apiClass): void
     {
-        if ($this->findApiAnnotationIndex($phpCsFile, $stackPointer)) {
+        $apiAnnotationIndex = $this->findApiAnnotationIndex($phpCsFile, $stackPointer);
+        if ($apiAnnotationIndex) {
+            if ($apiClass !== static::API_CONFIG) {
+                $this->assertInheritDoc($phpCsFile, $stackPointer);
+            }
+
             return;
         }
 
         $fix = $phpCsFile->addFixableError('@api annotation is missing', $stackPointer, 'ApiAnnotationMissing');
-
         if (!$fix) {
             return;
         }
@@ -316,5 +152,162 @@ class DocBlockApiAnnotationSniff implements Sniff
         }
 
         $phpCsFile->fixer->endChangeset();
+    }
+
+    /**
+     * Asserts that {@inheritDoc} is used for concrete class, and must not be used for interface.
+     *
+     * @param \PHP_CodeSniffer\Files\File $phpCsFile
+     * @param int $stackPointer
+     *
+     * @return void
+     */
+    protected function assertInheritDoc(File $phpCsFile, int $stackPointer): void
+    {
+        if (!$this->isInterface($phpCsFile, $stackPointer)) {
+            $this->assertInheritDocTag($phpCsFile, $stackPointer);
+
+            return;
+        }
+
+        $docCommentOpenerPosition = $phpCsFile->findPrevious(T_DOC_COMMENT_OPEN_TAG, $stackPointer);
+        if (!$docCommentOpenerPosition) {
+            return;
+        }
+
+        $tokens = $phpCsFile->getTokens();
+        $docCommentClosingPosition = $tokens[$docCommentOpenerPosition]['comment_closer'];
+        if (!$docCommentClosingPosition) {
+            return;
+        }
+
+        for ($i = $docCommentOpenerPosition + 1; $i < $docCommentClosingPosition; $i++) {
+            if (strpos($tokens[$i]['content'], '@inheritDoc') === false) {
+                continue;
+            }
+
+            $phpCsFile->addError(static::INHERIT_DOC . ' is only for concrete classes, the interfaces should contain the specification themselves.', $i, 'InvalidInheritDoc');
+
+            break;
+        }
+    }
+
+    /**
+     * @param \PHP_CodeSniffer\Files\File $phpCsFile
+     * @param int $stackPointer
+     *
+     * @return bool
+     */
+    protected function isInterface(File $phpCsFile, int $stackPointer): bool
+    {
+        $interfaceIndex = $phpCsFile->findPrevious(T_INTERFACE, $stackPointer);
+        if (!$interfaceIndex) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param \PHP_CodeSniffer\Files\File $phpCsFile
+     * @param int $stackPointer
+     *
+     * @return bool
+     */
+    protected function isConstructor(File $phpCsFile, int $stackPointer): bool
+    {
+        $methodNameIndex = $phpCsFile->findNext(T_STRING, $stackPointer);
+
+        $tokens = $phpCsFile->getTokens();
+
+        return $tokens[$methodNameIndex]['content'] === '__construct';
+    }
+
+    /**
+     * @param \PHP_CodeSniffer\Files\File $phpCsFile
+     * @param int $stackPointer
+     *
+     * @return void
+     */
+    protected function assertInheritDocTag(File $phpCsFile, int $stackPointer): void
+    {
+        $docCommentOpenerPosition = $phpCsFile->findPrevious(T_DOC_COMMENT_OPEN_TAG, $stackPointer);
+        if (!$docCommentOpenerPosition) {
+            return;
+        }
+
+        $tokens = $phpCsFile->getTokens();
+        $docCommentClosingPosition = $tokens[$docCommentOpenerPosition]['comment_closer'];
+        if (!$docCommentClosingPosition) {
+            return;
+        }
+
+        $hasInheritDoc = false;
+        for ($i = $docCommentOpenerPosition + 1; $i < $docCommentClosingPosition; $i++) {
+            if (stripos($tokens[$i]['content'], '@inheritDoc') === false) {
+                continue;
+            }
+
+            $hasInheritDoc = true;
+
+            break;
+        }
+
+        if ($hasInheritDoc) {
+            return;
+        }
+
+        $fix = $phpCsFile->addFixableError(static::INHERIT_DOC . ' missing for API method.', $docCommentOpenerPosition, 'InheritDocMissing');
+        if (!$fix) {
+            return;
+        }
+
+        $lastTokenOnLine = $this->lastTokenOnLine($tokens, $docCommentOpenerPosition);
+        $firstTokenOnNextLine = $this->firstTokenOnLine($tokens, $lastTokenOnLine + 1);
+
+        if (!$firstTokenOnNextLine) {
+            $phpCsFile->addErrorOnLine('Cannot fix missing inheritDoc tag', $stackPointer, 'InheritDocNotFixable');
+
+            return;
+        }
+
+        $phpCsFile->fixer->beginChangeset();
+
+        $phpCsFile->fixer->addContent($firstTokenOnNextLine, str_repeat(' ', 4) . ' * ' . static::INHERIT_DOC);
+        $phpCsFile->fixer->addNewline($firstTokenOnNextLine);
+
+        $phpCsFile->fixer->endChangeset();
+    }
+
+    /**
+     * @param array $tokens
+     * @param int $currentIndex
+     *
+     * @return int
+     */
+    protected function lastTokenOnLine(array $tokens, int $currentIndex): int
+    {
+        $line = $tokens[$currentIndex]['line'];
+        while ($tokens[$currentIndex + 1]['line'] === $line) {
+            $currentIndex++;
+        }
+
+        return $currentIndex;
+    }
+
+    /**
+     * @param array $tokens
+     * @param int $currentIndex
+     *
+     * @return int
+     */
+    protected function firstTokenOnLine(array $tokens, int $currentIndex): int
+    {
+        $line = $tokens[$currentIndex]['line'];
+        while ($tokens[$currentIndex - 1]['line'] === $line) {
+            $currentIndex--;
+        }
+
+        return $currentIndex;
     }
 }
