8ad73d4da6ae3cd846a4ce736c43536f24a67599
mscherer <mark.scherer@spryker.com>
2020-05-14 12:49:41 +0200

    Adjust docblock help.

--- Spryker/Tools/Traits/CommentingTrait.php
+++ Spryker/Tools/Traits/CommentingTrait.php
@@ -50,9 +50,9 @@ trait CommentingTrait
     }
 
     /**
-     * Allow \Foo\Bar[] to pass as array.
+     * Allow \Foo\Bar[] or array<\Foo\Bar> to pass as array.
      *
-     * @param array $docBlockTypes
+     * @param string[] $docBlockTypes
      *
      * @return bool
      */
@@ -60,6 +60,24 @@ trait CommentingTrait
     {
         foreach ($docBlockTypes as $docBlockType) {
             if (strpos($docBlockType, '[]') !== false || strpos($docBlockType, 'array<') === 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks for ...<\Foo\Bar>.
+     *
+     * @param string[] $docBlockTypes
+     *
+     * @return bool
+     */
+    protected function containsIterableSyntax(array $docBlockTypes): bool
+    {
+        foreach ($docBlockTypes as $docBlockType) {
+            if (strpos($docBlockType, '<') !== false) {
                 return true;
             }
         }
