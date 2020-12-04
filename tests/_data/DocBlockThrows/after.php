70af75143134880b0f698ea3e064c63b33a96e1c
Milos Levacic <milos@levacic.net>
2020-12-04 23:34:46 +0100

    Add failing tests for `DocBlockThrows.ThrowMissing`
    The "before" stub has two exceptions that should be documented using `@throws`
    annotations - the `RangeException` and the `LogicException` - whereas the other
    two (`DomainException` and `RuntimeException`) are thrown from nested scopes and
    shouldn't be part of the containing method's docblock.
    
    However, without the fix, this sniff reports four missing `@throws` annotations.

---
+++ tests/_data/DocBlockThrows/after.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types = 1);
+
+namespace Spryker;
+
+use Closure;
+use DomainException;
+use LogicException;
+use RangeException;
+use RuntimeException;
+
+class InvalidThrowSniff
+{
+    /**
+     * Return a closure that throws a runtime exception.
+     *
+     * @param bool $throw Whether to throw immediately.
+     * @throws LogicException
+     * @throws RangeException
+     * @return Closure
+     */
+    public function foo(bool $throw): Closure
+    {
+        if ($throw) {
+            throw new RangeException();
+        }
+
+        $foo = fn($value) => $value === true ? throw new DomainException() : null;
+
+        $bar = static function (): void {
+            throw new RuntimeException();
+        };
+
+        throw new LogicException();
+    }
+}
