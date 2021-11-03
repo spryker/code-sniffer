f3cd8346ff5d768b7fc9a06eaeb6b03fa58ecf8d
mscherer <mark.scherer@spryker.com>
2021-11-03 18:08:02 +0100

    Add Spryker.Commenting.DocBlockReturnNull

---
+++ tests/_data/DocBlockReturnNull/before.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types = 1);
+
+namespace Spryker;
+
+class FixMe
+{
+    /**
+     * @return mixed
+     */
+    public function thisIsFine()
+    {
+        return null;
+    }
+
+    /**
+     * @return static Missing null here
+     */
+    public function parse()
+    {
+        if ($this->foo()) {
+            return null;
+        }
+
+        return new static();
+    }
+}
