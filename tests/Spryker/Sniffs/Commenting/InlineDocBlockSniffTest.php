ac401b12d2faa0c2d8479c00af666e9d0ffbee93
mscherer <mark.scherer@spryker.com>
2021-12-01 22:34:57 +0100

    Fix inline docblock sniff.

---
+++ tests/Spryker/Sniffs/Commenting/InlineDocBlockSniffTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * MIT License
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace CodeSnifferTest\Spryker\Sniffs\Commenting;
+
+use CodeSnifferTest\TestCase;
+use Spryker\Sniffs\Commenting\InlineDocBlockSniff;
+
+class InlineDocBlockSniffTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testDocBlockConstSniffer(): void
+    {
+        $this->assertSnifferFindsErrors(new InlineDocBlockSniff(), 1);
+    }
+
+    /**
+     * @return void
+     */
+    public function testDocBlockConstFixer(): void
+    {
+        $this->assertSnifferCanFixErrors(new InlineDocBlockSniff());
+    }
+}
---
+++ tests/_data/InlineDocBlock/after.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types = 1);
+
+namespace Spryker;
+
+class FixMe
+{
+    public function test()
+    {
+        /**
+         * A collection of indexed columns. The key is the column name
+         * (concatenated with a comma in the case of multi-col index).
+         *
+         * @var array<string, array<string>> $_indices
+         */
+        $_indices = [];
+
+        /*
+         * This is not a docblock;
+         * But a multiline comment
+         */
+        $foo = $_indices['x'];
+
+        /** @var string|null $bar */
+        $bar = $_indices['y'];
+
+        /** @var string $foo */
+        $foo = $_indices['z'];
+    }
+}
