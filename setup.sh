17505d8367599aafaef80c013ef67833870735ee
Viktor Szépe <viktor@szepe.net>
2022-06-23 08:20:35 +0000

    Remove Composer tool

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Make sure this file is executable
-# chmod +x setup.sh
-
-# Download composer phar if not available yet
-[ ! -f composer.phar ] && curl -sS https://getcomposer.org/installer | php
