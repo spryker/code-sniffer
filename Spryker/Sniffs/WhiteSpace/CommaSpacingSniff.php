c82fe38bf9f1f93f5c0911a2f5bab10f65e868af
mscherer <mark.scherer@spryker.com>
2023-01-12 16:14:04 +0100

    Add docs.

--- Spryker/Sniffs/WhiteSpace/CommaSpacingSniff.php
+++ Spryker/Sniffs/WhiteSpace/CommaSpacingSniff.php
@@ -5,7 +5,7 @@
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
-namespace Spryker\Sniffs\WhiteSpace;
+namespace Spryker\Sniffs\WhiteSpace\Foo;
 
 use PHP_CodeSniffer\Files\File;
 use PHP_CodeSniffer\Sniffs\Sniff;
